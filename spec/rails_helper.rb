20e0dc9fa558fe69e3fb68017eacabcaeeeed4c8
Aotokitsuruya <contact@aotoki.me>
2024-10-07 11:04:30 +0800

    Bump rubocop-rails to 2.26.2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ require 'faker'
 require 'factory_bot'
 require 'sidekiq/testing'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
